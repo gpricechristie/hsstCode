b9f5bbb98c6f94a38d6cc67cb78352cb1b44d996
gpricechristie <50412644+gpricechristie@users.noreply.github.com>
2019-05-17 11:52:15 +0100

    Add files via upload
    Example SQL and SPARQL queries again the dvdRental database

---
+++ dvdRental.sql
@@ -0,0 +1,9 @@
+﻿SELECT cust.first_name, cust.last_name,
+film.title
+FROM customer cust
+JOIN rental rent on cust.customer_id = rent.customer_id
+JOIN inventory inv on rent.inventory_id = inv.inventory_id
+JOIN film film on film.film_id = inv.film_id
+WHERE film.title ILIKE '%Whale%'
+ORDER BY cust.first_name, cust.last_name,
+film.title
